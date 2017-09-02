11645df7522ba4e9065071e66a16037ab62b4156
Tu nombre <t@ejemplo.com>
2017-09-02 16:43:46 -0300

    Mi app Django Girls, primer commit

---
+++ Marcela.py
@@ -0,0 +1,15 @@
+def hi():
+	print("Hi there!")
+	print("How are yoy?")
+hi()
+def hi(name):
+	if name=="Ola":
+		print("Hi Ola!")
+	elif name=="Sonja":
+		print("Hi Sonja!")
+	else:
+		print("Hi anonymous!")
+
+hi("Marcela")
+
+
